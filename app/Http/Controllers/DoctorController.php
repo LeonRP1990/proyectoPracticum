9f3c3f6ba184d4dbbc0e76326945b972687c2e8a
LeonRP1990 <drleon8@utpl.edu.ec>
2025-01-12 23:43:43 -0500

    MVC Citas Médicas y Enfermedades

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $doctors = Doctor::all();
+        return view('doctors.index', compact('doctors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('doctors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'requered|string|max:255',
+            'specialty' => 'requered|string|max:255',
+            'contact' => 'requered|string|max:255',
+        ]);
+
+        Doctor::create($request->all());
+        return redirect()->route('doctors.index')->with('success','Doctor creado satisfactoriamente');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Doctor $doctor)
+    {
+        return view('doctors.show', compact('doctor'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Doctor $doctor)
+    {
+        return view('doctors.edit', compact('doctor'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Doctor $doctor)
+    {
+        $request->validate([
+            'name' => 'requered|string|max:255',
+            'specialty' => 'requered|string|max:255',
+            'contact' => 'requered|string|max:255',
+        ]);
+
+        $doctor->update($request->all());
+        return redirect()->route('doctors.index')->with('success','Doctor actualizados satisfactoriamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Doctor $doctor)
+    {
+        $doctor->delete();
+        return redirect()->route('doctors.index')->with('success','Doctor eliminado satisfactoriamente');
+
+    }
+}
